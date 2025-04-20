052747179c1d6d2b171867592ad2712ca0a22e30
Dener Garcia <dener.criarbr@gmail.com>
2025-04-20 18:53:02 +0000

    feat: add o props.children in AuthProvider

--- src/context/auth.js
+++ src/context/auth.js
@@ -4,18 +4,22 @@ import { createContext, useState } from "react";
 const AuthContext = createContext({})
 
 // cria uma function de contexto global
-function AuthProvider(props){
+function AuthProvider(props) {
 
     // criando um useState de uso global
     const [user, setUser] = useState({})
 
-// o que a function vai retornar pelo .Provider é o objeto dentro de value
+    // o return AuthContext.Providerretornar pelo .Provider o objeto dentro de value para se tornar de uso global
+    // o props.children vai pegar o componente do app.js que envolvemos pelo AuthProvider no caso o Routes e devolver para o provider poder retornar a aplicacao, sem isso nao funciona
+
     return <AuthContext.Provider value={
         {
             user,
             setUser
-        }
-    }>
+        }}>
+
+        {props.children}
+
     </AuthContext.Provider>
 }
 
